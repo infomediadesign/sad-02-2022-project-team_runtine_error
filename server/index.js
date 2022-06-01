884d64a7dc2a9ef944e986f89c977bb8c37b542f
Abir Banerjee <abir.banerjee@gmail.com>
2022-06-01 11:33:08 +0200

    some static frontend pages for test

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(express.static('www'));
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
