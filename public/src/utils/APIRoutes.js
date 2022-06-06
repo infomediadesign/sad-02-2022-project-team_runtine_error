a51335e1db6c7cd9f3017d84939d59c85945639f
Shivani Lohokare <shivanilohokaree@gmail.com>
2022-06-06 15:15:08 +0200

    Register Page Styling and restructuring of frontend

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -3,5 +3,6 @@ export const registerRoute = `${host}/register`;
 export const loginRoute = `${host}/login`;
 export const SetAvatarRouter = `${host}/setAvatar`;
 //^ get all contacts from database 
+export const ChatRouter = `${host}/Chat`;
 export const allUsersRoute = `${host}/allusers`;
 
