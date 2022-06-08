625acf8ddea41711e0d24ffa25f6804ce79d40f8
Enes AGIR <enes.agir.7@gmail.com>
2022-06-08 15:09:12 +0200

    chat functionality frontend

--- public/src/components/Messages.jsx
+++ public/src/components/Messages.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 export default function Messages() {
-  return (
-    <Container>Messages</Container>
-  )
+    return (
+        <Container>Messages</Container>
+    )
 }
 
 const Container = styled.div`
